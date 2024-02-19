6d77b2c4fd2907c01170edcbc6cb2a42643e1737
CodeAnarchist <144830359+CodeAnarchist@users.noreply.github.com>
2024-02-19 19:02:31 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 @echo off
-
-start "API Server" cmd /c "node backend/server.js"
-
-timeout /t 5 /nobreak >nul
-
+npm list express mongoose body-parser bcrypt express-rate-limit helmet cors >nul 2>&1
+if %errorlevel% neq 0 (
+    npm install express mongoose body-parser bcrypt express-rate-limit helmet cors
+)
+npm install >nul 2>&1
 start "React App" cmd /c "npm start"
+timeout /t 1 /nobreak >nul
+start "API Server" cmd /c "node backend/server.js"
