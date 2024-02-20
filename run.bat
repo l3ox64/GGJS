3b4ee6cde2f0507836861c4936d0d9516efea6d5
l3ox64 <l3ox0705@gmail.com>
2024-02-20 10:15:39 -0800

    dependency fix

--- run.bat
+++ run.bat
@@ -1,10 +1,6 @@
 @echo off
-npm install
-npm list express mongoose body-parser bcrypt express-rate-limit helmet cors >nul 2>&1
-if %errorlevel% neq 0 (
-    npm install express mongoose body-parser bcrypt express-rate-limit helmet cors
-)
+
 npm install >nul 2>&1
 start "React App" cmd /c "npm start"
 timeout /t 1 /nobreak >nul
-start "API Server" cmd /c "node backend/server.js"
+start "API Server" cmd /c "node ./backend/server.js"
