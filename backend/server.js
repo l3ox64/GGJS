1414b9553dd90022b1c03b3ce4995df7504022cf
CodeAnarchist <leonardo300506@gmail.com>
2024-02-29 17:35:36 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -12,51 +12,67 @@ const { logError, logException } = require('./methods/internalmethod');
 const DB_CHK_INTERVAL = 1000*60*30;
 const TIMEOUT_TIME=10000 //tempo timeout in caso di errore 
 const PORT = process.env.API_PORT || 3001;
+const MAX_DB_CONNECTION_RETRIES = 5;
 
+
+const connectWithRetry = async () => {
+  let retryCount = 0;
+  const retryInterval = 5000; // millisecondi
+  while (retryCount < MAX_DB_CONNECTION_RETRIES) {
+    try {
+      await mongoose.connect('mongodb://localhost:27017/GGJSDB', {});
+      break;
+    } catch (error) {
+      console.error(`Errore durante la connessione al database: ${error.message}`);
+      retryCount++;
+      console.log(`Ritentando la connessione (tentativo ${retryCount}/${MAX_DB_CONNECTION_RETRIES})...`);
+      await new Promise(resolve => setTimeout(resolve, retryInterval));
+    }
+  }
+  if (retryCount === MAX_DB_CONNECTION_RETRIES) {
+    console.error(`Impossibile connettersi al database dopo ${MAX_DB_CONNECTION_RETRIES} tentativi.`);
+    process.exit(1);
+  }
+};
+
+connectWithRetry();
 
 try {
-  mongoose.connect('mongodb://localhost:27017/GGJSDB', {});
-  console.log('Connesso al database MongoDB');
-} catch (error) {
-  console.error('Errore durante la connessione al database MongoDB:', error);
-  process.exit(1);
-}
-try{
-setInterval(runTestWithTiming, DB_CHK_INTERVAL);
-configureApp(app);
-var server = app.listen(PORT, () => {
-  console.log(`Server in ascolto sulla porta ${PORT}`);
-});
+  setInterval(runTestWithTiming, DB_CHK_INTERVAL);
+  configureApp(app);
+  var server = app.listen(PORT, () => {
+    console.log(`Server in ascolto sulla porta ${PORT}`);
+  });
 } catch (error) {
   console.error('Errore durante l\'avvio del server:', error);
   process.exit(1);
 }
 
-process.on('uncaughtException', handleUncaughtException); //blocco nel caso di uncaughtException
+process.on('uncaughtException', handleUncaughtException); // blocco nel caso di uncaughtException
 process.on('unhandledRejection', handleUnhandledRejection); // " unhandledRejection
 process.on('SIGINT', handleSIGINT);
 
-
-function handleUncaughtException(err) { 
+function handleUncaughtException(err) {
   const stackTrace = err.stack || 'No stack trace available';
   if (err instanceof TypeError) {
-  console.error(`TypeError: ${err.message}`);
-  logException('TypeError', err.message, stackTrace);
-  }else{
-  logException('Uncaught Exception', err.message, stackTrace);
-  console.error(`Errore non gestito: ${err.message}`);
-  server.close(() => {
-    setTimeout(() => {
-      server.listen(PORT, () => {
-        console.log(`Server riavviato dopo errore: ${err.message}`);
-      });
-    }, TIMEOUT_TIME);
-  });
-  sendNotificationEmail('Errore non gestito nel server', err.message);
+    console.error(`TypeError: ${err.message}`);
+    logException('TypeError', err.message, stackTrace);
+  } else {
+    logException('Eccezione non gestita', err.message, stackTrace);
+    console.error(`Errore non gestito: ${err.message}`);
+    server.close(() => {
+      setTimeout(() => {
+        server.listen(PORT, () => {
+          console.log(`Server riavviato dopo errore: ${err.message}`);
+        });
+      }, TIMEOUT_TIME);
+    });
+    sendNotificationEmail('Errore non gestito nel server', err.message);
+  }
 }
-}
+
 function handleUnhandledRejection(reason) {
-  const stackTrace = reason.stack || 'No stack trace available';
+  const stackTrace = reason.stack || 'Nessuna traccia disponibile';
   logException('Unhandled Rejection', reason, stackTrace);
   console.error(`Unhandled Rejection: ${reason}`);
   server.close(() => {
@@ -66,14 +82,21 @@ function handleUnhandledRejection(reason) {
       });
     }, TIMEOUT_TIME);
   });
-  sendNotificationEmail('Unhandled Rejection nel server', reason);
+  sendNotificationEmail('Unhandled Rejection non gestito nel server', reason);
 }
+
 function handleSIGINT() {
   try {
+    if (server) {
+      console.log('Stop del server...');
+      server.close();
+      console.log('Server stoppato con successo.');
+    }
     mongoose.connection.close();
-    console.log('Connessione al database chiusa correttamente');
+    console.log('Connessione al database chiusa con successo.');
     process.exit(0);
   } catch (error) {
-    console.log("Errore durante la chiusura della connessione al database:", error);
+    console.error('Errore durante la chiusura:', error);
+    process.exit(1);
   }
 }
